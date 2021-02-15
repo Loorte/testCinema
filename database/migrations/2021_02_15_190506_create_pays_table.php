dc5085c2eb17bbe741dc1341c06affa07c67beac
Loorte <sokolov_mm@m2-soft.ru>
2021-02-16 00:07:36 +0500

    Добавил миграцию к #4

---
+++ database/migrations/2021_02_15_190506_create_pays_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pays', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('film_session_id');
+            $table->integer('line');
+            $table->integer('point');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pays');
+    }
+}
