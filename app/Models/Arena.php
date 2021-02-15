8c3544f16f96ec31791ef13d5df8b54cb83c19a1
Loorte <sokolov_mm@m2-soft.ru>
2021-02-15 23:42:07 +0500

    добавил миграцию к #1

---
+++ app/Models/Arena.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Arena extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'name',
+      'count_line',
+      'count_point',
+      'is_delete'
+    ];
+}
---
+++ database/migrations/2021_02_15_174427_create_arenas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArenasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arenas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->text('name');
+            $table->integer('count_line')->nullable()->default(3);
+            $table->integer('count_point')->nullable()->default(5);
+            $table->boolean('is_delete')->nullable()->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arenas');
+    }
+}
