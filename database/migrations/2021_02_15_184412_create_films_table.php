e73bbe25de87b2f8bf5563ee49967869f0048dce
Loorte <sokolov_mm@m2-soft.ru>
2021-02-15 23:46:29 +0500

    Добавил миграцию к #2

---
+++ database/migrations/2021_02_15_184412_create_films_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('name');
+            $table->integer('video_time')->nullable()->default(0); //Время в минутах
+            $table->integer('is_delete')->nullable()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
